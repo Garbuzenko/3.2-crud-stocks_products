522c3b95c75abd86350430f0de09afdf42c360b3
mike.garbuzenko@gmail.com <mike.garbuzenko@gmail.com>
2022-08-27 22:32:38 +0300

    Initial commit

---
+++ logistic/views.py
@@ -0,0 +1,22 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.viewsets import ModelViewSet
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_field = ['title', 'description']
+    search_fields = ['title', 'description']
+    # при необходимости добавьте параметры фильтрации
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    # filterset_field = ['title', 'descriptions']
+    # при необходимости добавьте параметры фильтрации
